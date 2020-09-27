2fcd0d7c96bdc071755c5b248e67da77f5fd521f
positive-proc-6135 <haydenmatz97@gmail.com>
2020-09-27 19:22:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,8 @@
 var playlist = { ACDC: 'Thunderstruck'};
+function updatePlaylist( list, 'artistName', 'songTitle'){
+  function updatePlaylist( list, 'artist', 'name'){
+    list = playlist
+     playlist.[artistName: 'songTitle']
+  }
+
+  
