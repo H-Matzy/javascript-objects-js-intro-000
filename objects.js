446a61415dfa1098e78c5bce8a6fec029a097dab
positive-proc-6135 <haydenmatz97@gmail.com>
2020-09-27 19:30:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist( list, artistName, songTitle){
     list = playlist
      playlist[artistName] = songTitle
   }
+function removeFromPlaylist(list,artistName){
+  list = playlist
+    delete playlist[artistName]
+}
